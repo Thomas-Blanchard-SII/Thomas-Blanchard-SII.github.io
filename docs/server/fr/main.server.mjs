0f31bf668c3835ca24721ad95054323aa567a864
Thomas Blanchard <thomas.blanchard@sii.fr>
2024-06-18 16:06:14 +0200

    delete

--- docs/server/fr/main.server.mjs
+++
@@ -1,3 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-T7A72GJ4.mjs";import"./chunk-6UOHRYA5.mjs";import"./chunk-Z42Z6FUR.mjs";export{a as default};
-/**i18n:81bf1f4b186b365d9dda59970ad302165f2de2a0fb7fee7ae3b3e76f4eda41f3*/
